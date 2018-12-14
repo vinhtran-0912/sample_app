34c50cb1158450b639fd375e0f9b64f8f5707deb
[DN_OE18_Ruby]TranNgocVinh <vinhtran09121997@gmail.com>
2018-12-14 13:13:30 +0700

    push chapter7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
-  def new; end
+  before_action :load_user, only: :show
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".welcome"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".error"
+    redirect_to @user
+  end
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end

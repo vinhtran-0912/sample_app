997d3b4ab3236b1c5cf74025f1c11bca736a1887
[DN_OE18_Ruby]TranNgocVinh <vinhtran09121997@gmail.com>
2018-12-19 14:35:24 +0700

    chapter9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
-    @user = User.find_by email: params[:session][:email].downcase
+    user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate(params[:session][:password])
       log_in user
+      check_rememember user
       redirect_to user
     else
       flash.now[:danger] = t ".error_messages"
@@ -14,7 +14,16 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
+    flash[:success] = t ".logout_messages"
     redirect_to root_path
+  end
+
+  def check_rememember user
+    if params[:session][:remember_me] == Settings.session_controller.remember_1
+      remember user
+    else
+      forget user
+    end
   end
 end
